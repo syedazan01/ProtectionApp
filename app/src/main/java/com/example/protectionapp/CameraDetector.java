630992af8a10b58ad91c2582afa9ccac98cc838a
appentusiOS <apple@appentus.me>
2020-07-20 18:29:37 +0530

    Zaid commit

--- app/src/main/java/com/example/protectionapp/CameraDetector.java
+++ app/src/main/java/com/example/protectionapp/CameraDetector.java
@@ -1,14 +1,39 @@
 package com.example.protectionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CameraDetector extends AppCompatActivity {
-
+Toolbar toolbar;
+ImageView ivBack;
+TextView tvToolbarTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_detector);
+        setSupportActionBar(toolbar);
+        addViews();
+        initActions();
+    }
+
+    private void initActions() {
+    ivBack.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            onBackPressed();
+        }
+    });
+    }
+
+    private void addViews() {
+        toolbar=findViewById(R.id.toolbar);
+        ivBack=findViewById(R.id.ivBack);
+        tvToolbarTitle=findViewById(R.id.tvToolbarTitle);
+        tvToolbarTitle.setText("Hidden Camera Finder");
     }
 }
