d2ae751251ed1476d63d9ca940a72fd3d08b035b
appentusiOS <apple@appentus.me>
2020-07-28 22:18:14 +0530

    Zaid: Firebase

--- app/src/main/java/com/example/protectionapp/activites/CameraDetector.java
+++ app/src/main/java/com/example/protectionapp/activites/CameraDetector.java
@@ -3,20 +3,29 @@ package com.example.protectionapp.activites;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.protectionapp.R;
 import com.example.protectionapp.utils.PrefManager;
 
 import static com.example.protectionapp.utils.AppConstant.ISNIGHTMODE;
 
-public class CameraDetector extends AppCompatActivity {
+public class CameraDetector extends AppCompatActivity implements SensorEventListener {
 Toolbar toolbar;
 ImageView ivBack;
 TextView tvToolbarTitle;
+SensorManager sensorManager;
+Sensor magnetometerSensor;
+ProgressBar meterView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +34,19 @@ TextView tvToolbarTitle;
         else
             setTheme(R.style.AppTheme_Base_Light);
         setContentView(R.layout.activity_camera_detector);
-        setSupportActionBar(toolbar);
+       sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+       magnetometerSensor=sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+       if(magnetometerSensor==null)
+           Toast.makeText(this, "No Magnetometer sensor detector", Toast.LENGTH_SHORT).show();
+    setSupportActionBar(toolbar);
         addViews();
         initActions();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,magnetometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     private void initActions() {
@@ -43,6 +62,29 @@ TextView tvToolbarTitle;
         toolbar=findViewById(R.id.toolbar);
         ivBack=findViewById(R.id.ivBack);
         tvToolbarTitle=findViewById(R.id.tvToolbarTitle);
+        meterView=findViewById(R.id.meterView);
+        meterView.setMax(500);
         tvToolbarTitle.setText("Hidden Camera Detector");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+if(sensorEvent.sensor.getType() ==Sensor.TYPE_MAGNETIC_FIELD)
+{
+    String sensorValue=sensorEvent.values[0]+" "+ sensorEvent.values[1]+ " "+sensorEvent.values[2];
+    meterView.setProgress((int)sensorEvent.values[0]);
+}
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
