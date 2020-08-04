a844e48d636311681414c968e8ec3cc6f2b5e40f
Zaid050297 <zaidmansoori34@gmail.com>
2020-08-04 23:52:55 +0530

    Azan: all records design and helper class complete some java code pending ....

--- app/src/main/java/com/example/protectionapp/utils/PanHelperClass.java
+++ app/src/main/java/com/example/protectionapp/utils/PanHelperClass.java
@@ -1,4 +1,44 @@
 package com.example.protectionapp.utils;
 
 public class PanHelperClass {
+    String fullName,fatherName,dateOfBirth,PermanentAccountNumber;
+
+    public PanHelperClass(String fullName, String fatherName, String dateOfBirth, String permanentAccountNumber) {
+        this.fullName = fullName;
+        this.fatherName = fatherName;
+        this.dateOfBirth = dateOfBirth;
+        PermanentAccountNumber = permanentAccountNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPermanentAccountNumber() {
+        return PermanentAccountNumber;
+    }
+
+    public void setPermanentAccountNumber(String permanentAccountNumber) {
+        PermanentAccountNumber = permanentAccountNumber;
+    }
 }
--- app/src/main/java/com/example/protectionapp/utils/views/BankHelperClass.java
+++ app/src/main/java/com/example/protectionapp/utils/views/BankHelperClass.java
@@ -1,4 +1,53 @@
 package com.example.protectionapp.utils.views;
 
 public class BankHelperClass {
+    String AccountHolderName,accountNumber,ifscCode,branchName,BankName;
+
+    public BankHelperClass(String accountHolderName, String accountNumber, String ifscCode, String branchName, String bankName) {
+        AccountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.ifscCode = ifscCode;
+        this.branchName = branchName;
+        BankName = bankName;
+    }
+
+    public String getAccountHolderName() {
+        return AccountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        AccountHolderName = accountHolderName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getIfscCode() {
+        return ifscCode;
+    }
+
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getBankName() {
+        return BankName;
+    }
+
+    public void setBankName(String bankName) {
+        BankName = bankName;
+    }
 }
