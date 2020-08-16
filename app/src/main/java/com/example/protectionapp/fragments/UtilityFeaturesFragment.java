0f972444369ea30ffbb83fffa8beb7acd5975492
Zaid050297 <zaidmansoori34@gmail.com>
2020-08-16 13:50:06 +0530

    Azan:screenshot implemented

--- app/src/main/java/com/example/protectionapp/fragments/UtilityFeaturesFragment.java
+++ app/src/main/java/com/example/protectionapp/fragments/UtilityFeaturesFragment.java
@@ -21,51 +21,17 @@ import com.example.protectionapp.utils.AppConstant;
 import com.example.protectionapp.utils.PrefManager;
 import com.example.protectionapp.utils.Utils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UtilityFeaturesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UtilityFeaturesFragment extends Fragment implements FloatingWindowService.OnFabClick {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
     public static FloatingWindowService.OnFabClick onFabClick = null;
     private Switch swEnableLauncher;
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UtilityFeaturesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UtilityFeaturesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UtilityFeaturesFragment newInstance(String param1, String param2) {
-        UtilityFeaturesFragment fragment = new UtilityFeaturesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
